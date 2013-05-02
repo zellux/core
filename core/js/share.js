ea3c2e408f9e69764c2c251a52af84e50834f1f0
Michael Gapczynski <mtgap@owncloud.com>
2013-05-02 11:59:11 -0400

    Fix accidental deletion of if

--- core/js/share.js
+++ core/js/share.js
@@ -165,6 +165,7 @@ OC.Share={
 					linksAllowed = true;
 				}
 			}});
+			if (link && linksAllowed) {
 				html += '<div id="link">';
 				html += '<input type="checkbox" name="linkCheckbox" id="linkCheckbox" value="1" /><label for="linkCheckbox">'+t('core', 'Share with link')+'</label>';
 				html += '<br />';
